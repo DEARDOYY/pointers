c296ac6768435d9963d7b67f470a37bdb3701e67
Pinya Aungkanarungrut <88120284+DEARDOYY@users.noreply.github.com>
2024-08-21 22:34:36 +0700

    update func

--- pointers.go
+++ pointers.go
@@ -1,3 +1,15 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	age := 32 // regular variable
+	fmt.Println("Age: ", age)
+
+	adultYears := getAdultYears(age)
+	fmt.Println(adultYears)
+}
+
+func getAdultYears(age int) int {
+	return age - 18
+}
