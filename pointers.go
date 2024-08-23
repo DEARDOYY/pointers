f7a99e2c4f00672cdb0607cf30df00236b1d4f04
Pinya Aungkanarungrut <88120284+DEARDOYY@users.noreply.github.com>
2024-08-23 22:31:14 +0700

    update pointer

--- pointers.go
+++ pointers.go
@@ -4,12 +4,17 @@ import "fmt"
 
 func main() {
 	age := 32 // regular variable
-	fmt.Println("Age: ", age)
 
-	adultYears := getAdultYears(age)
+	var agePointer *int
+
+	agePointer = &age
+
+	fmt.Println("Age: ", *agePointer)
+
+	adultYears := getAdultYears(agePointer)
 	fmt.Println(adultYears)
 }
 
-func getAdultYears(age int) int {
-	return age - 18
+func getAdultYears(age *int) int {
+	return *age - 18
 }
